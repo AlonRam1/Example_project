8e752b918d464b3265a90554bb5a2be8b6cab640
AlonRam <alon.ram.rimmler@gmail.com>
2025-04-03 11:13:25 +0300

    added a function

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public void randomFunction(){
+        System.out.println("aaaa");
+
+    }
     public static void main(String[] args) {
         System.out.println("example");
     }
